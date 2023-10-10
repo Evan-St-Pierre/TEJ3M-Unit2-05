acdc8bc4fafe0fadd4fec6b7e66df930a6c738c6
Evan-St-Pierre <144252383+Evan-St-Pierre@users.noreply.github.com>
2023-10-10 11:00:25 -0400

    final

---
+++ Main.ino
@@ -0,0 +1,25 @@
+/*
+  Created by: Evan St-Pierre
+  Created on: Sept 2023
+  Rotates a servo 90 degrees
+*/
+
+#include <Servo.h>
+
+Servo servo;
+
+void setup() {
+  servo.attach(9);
+  servo.write(0);
+  delay(2000);
+  
+}
+
+void loop() {
+  
+  servo.write(90);
+    delay(1000);
+  servo.write(0);
+    delay(1000);
+    
+}
